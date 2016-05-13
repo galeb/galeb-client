cb9c3e11b1e06719e2853c573a55c4249949e8aa
joao.borsani <joao.borsani@corp.globo.com>
2016-05-13 14:48:44 -0300

    initial commit :)

---
+++ README.md
@@ -0,0 +1,24 @@
+## Galeb Client (CLI)
+
+A easy wrapper CLI to use the Galeb Manager.
+
+### Build
+
+```bash
+go build -o galeb
+```
+
+### LICENSE
+
+```Copyright
+Copyright (c) 2014-2015 Globo.com - All rights reserved.
+
+ This source is subject to the Apache License, Version 2.0.
+ Please see the LICENSE file for more information.
+
+ Unless required by applicable law or agreed to in writing, software
+ distributed under the License is distributed on an "AS IS" BASIS,
+ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and
+ limitations under the License.
+ ```
---
+++ main.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"crypto/tls"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"
+)
+
+type jsonData struct {
+	Embedded `json:"_embedded"`
+}
+
+type Embedded struct {
+	PoolData []Pool `json:"pool"`
+}
+
+type Pool struct {
+	Id     int    `json:"id"`
+	Name   string `json:"name"`
+	Status string `json:"_status"`
+}
+
+func main() {
+	host := os.Getenv("GALEB_HOST")
+	token := os.Getenv("GALEB_TOKEN")
+	if host == "" {
+		fmt.Println("GALEB_HOST undefined.")
+		return
+	}
+	if token == "" {
+		fmt.Println("GALEB_TOKEN undefined.")
+		return
+	}
+	url := host + os.Args[1]
+	pool := make([]Pool, 0)
+	data := jsonData{Embedded{pool}}
+
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Set("x-auth-token", token)
+	req.Header.Set("Content-Type", "application/json")
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	client := &http.Client{Transport: tr}
+	resp, err := client.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error while parsing file")
+		return
+	}
+
+	for _, pool := range data.PoolData {
+		fmt.Printf("Id = %v, Name = %v, Status = %v\n", pool.Id, pool.Name, pool.Status)
+	}
+}
